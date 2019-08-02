f498c32d8cb048d77f45194d5e2d36cdd7605be2
Kenta <kentat01@gmail.com>
2019-08-02 22:16:34 +0900

    Initial Commit

---
+++ PortraitEffectsMatteSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PortraitEffectsMatteSample
+//
+//  Created by 田中健太 on 2019/08/02.
+//  Copyright © 2019 田中健太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PortraitEffectsMatteSampleTests/PortraitEffectsMatteSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  PortraitEffectsMatteSampleTests.swift
+//  PortraitEffectsMatteSampleTests
+//
+//  Created by 田中健太 on 2019/08/02.
+//  Copyright © 2019 田中健太. All rights reserved.
+//
+
+import XCTest
+@testable import PortraitEffectsMatteSample
+
+class PortraitEffectsMatteSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
